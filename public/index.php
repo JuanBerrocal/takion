71ea6780b0b610f3d585d40fec88b85e4dbf0bfc
JuanBerrocal <juan.brudner@gmail.com>
2025-12-14 22:25:18 +0100

    index.php changed. symfony runtime not used

--- public/index.php
+++ public/index.php
@@ -9,9 +9,21 @@ use App\Kernel;
 //error_reporting(E_ALL);
 //ini_set('display_errors', 1);
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+# No used any longer because Synfony tries to load .env file
+# require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
+$env = $_SERVER['APP_ENV'] ?? getenv('APP_ENV') ?: 'prod';
+$debug = (bool) ($_SERVER['APP_DEBUG'] ?? getenv('APP_DEBUG') ?: false);
  
-return function (array $context) {
+$kernel = new Kernel($env, $debug);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
+
+/*return function (array $context) {
     //return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
     return new Kernel($_SERVER['APP_ENV'] ?? 'prod', (bool) ($_SERVER['APP_DEBUG'] ?? false));
-};
+};*/
