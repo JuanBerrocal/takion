30910ba5892760da8da3f46b9972fa844460b145
JuanBerrocal <juan.brudner@gmail.com>
2025-12-16 12:35:48 +0100

    debug messages disabled

--- public/index.php
+++ public/index.php
@@ -4,21 +4,19 @@
 use App\Kernel;
 use Symfony\Component\HttpFoundation\Request;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ** ONLY TO DEBUG
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-// ** ONLY FOR DEBUGGING
-//error_reporting(E_ALL);
-//ini_set('display_errors', 1);
 
-# No used any longer because Synfony tries to load .env file
+# No used any longer because Synfony tries to load .env file and symfony crashes when deployed in render.
 # require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
 $env = $_SERVER['APP_ENV'] ?? getenv('APP_ENV') ?: 'prod';
-$debug = (bool) ($_SERVER['APP_DEBUG'] ?? getenv('APP_DEBUG') ?: true);
+$debug = (bool) ($_SERVER['APP_DEBUG'] ?? getenv('APP_DEBUG') ?: false);
  
 /*$kernel = new Kernel($env, $debug);
 $request = Request::createFromGlobals();
@@ -26,7 +24,8 @@ $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);*/
 
-error_log('*** MENSAJE DE PRUEBA: index.php EJECUTADO ***'); 
+// ** ONLY TO DEBUG
+// error_log('*** MENSAJE DE PRUEBA: index.php EJECUTADO ***'); 
 
 try {
     $kernel = new Kernel($env, $debug);
@@ -48,31 +47,7 @@ try {
     http_response_code(500);
     echo "Internal Server Error. Revisa var/log/prod.log";
 }
-/*
-try {
-    $kernel = new Kernel($env, $debug);
-    $request = Request::createFromGlobals();
-    
-    try {
-        $response = $kernel->handle($request);
-        $response->send();
-        $kernel->terminate($request, $response);
-    } catch (\Throwable $e) {
-        // Captura errores durante la ejecución del Kernel
-        error_log('*** ERROR DURANTE HANDLE/TERMINATE ***');
-        error_log($e->__toString());
-        http_response_code(500);
-        echo 'Ocurrió un error interno. Revisa logs.';
-    }
 
-} catch (\Throwable $e) {
-    // Captura errores al inicializar el Kernel
-    error_log('*** ERROR DURANTE INICIALIZACION DEL KERNEL ***');
-    error_log($e->__toString());
-    http_response_code(500);
-    echo 'Ocurrió un error interno. Revisa logs.';
-}
-*/
 /*return function (array $context) {
     //return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
     return new Kernel($_SERVER['APP_ENV'] ?? 'prod', (bool) ($_SERVER['APP_DEBUG'] ?? false));
